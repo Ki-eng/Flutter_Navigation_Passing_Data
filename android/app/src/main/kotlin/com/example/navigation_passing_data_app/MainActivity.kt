1cfeabc10f57479ba0d7b1a31c09ba2142a85496
Kiran Saleem <saleemkiran1599@gmail.com>
2024-12-20 23:37:29 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/navigation_passing_data_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.navigation_passing_data_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/resultScreen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_passing_data_app/customCard.dart';
+import 'package:navigation_passing_data_app/resetQuizStatus.dart';
+import 'package:navigation_passing_data_app/scoreCard.dart';
+import 'customButton.dart';
+import 'scoreCard.dart';
+import 'quiz.dart';
+
+class Resultscreen extends StatelessWidget {
+  Resultscreen({this.score = 0, this.totalAttempted = 0});
+
+  final int score;
+  final int totalAttempted;
+  @override
+  Widget build(BuildContext context) {
+    Quiz quizScoreCard = Quiz();
+
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RESULT Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Customcard(
+              elevation: 10,
+              shadowColor: Colors.grey,
+              height: 200,
+              headingWidget: Text(
+                'Quiz Ended',
+                style: theme.textTheme.headlineMedium,
+              ),
+              bodyWidget: Text(
+                'Score $score/$totalAttempted',
+                style: theme.textTheme.headlineMedium,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomButton(
+                  onPress: () => Navigator.pop(
+                    context,
+                    Resetquizstatus(status: 'reset'),
+                  ),
+                  title: 'RESTART',
+                  backgroundColor: Colors.indigo,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+    ;
+  }
+}
